0713b454c553402f0a9ff30d394b54644f7c5ac5
cksun <1037134100@qq.com>
2018-12-21 19:28:27 +0800

    产品分类

--- clientsite/src/index.js
+++ clientsite/src/index.js
@@ -1,9 +1,13 @@
+// 存放的是这个项目的核心内容，也就是我们的主要工作区域。其中，index.js文件是和index.html进行关联的文件的唯一接口。
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; //将App.js的内容引入到index.js文件中
 import * as serviceWorker from './serviceWorker';
 
+// ReactDOM.render(）的作用是将<App/>的内容渲染到根“root”中去。
+// document.getElementById('root')中的"root"便是index.html中的"root"了，<App />便是引用页面内容了。在这里，<App />也可以写一些内容(结构,样式,逻辑)是整个项目的根组件，比如:
+// ReactDOM.render(<p >Hello World</p>, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- clientsite/src/pages/test.js
+++ clientsite/src/pages/test.js
@@ -1,25 +1,98 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+class Test extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/pages/test.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://www.baidu.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            百度
-          </a>
-        </header>
-      </div>
+      <div>
+            <SearchBar/>
+            <ProductTable products={PRODUCTS}/>
+        </div>
     );
   }
 }
 
-export default App;
+class SearchBar extends Component {
+  render() {
+      return (
+      <div>
+        <input type="text" placeholder="Search"/>
+        <p>
+            <input type="checkbox"/>{''}Only show products in stock
+        </p>
+       </div>
+    )
+  }
+}
+
+class ProductTable extends Component {
+  render() {
+    const rows = [];
+    let lastCategory = null;
+    this.props.products.forEach((product) => {
+        //要清楚我们需要的结构rows = [<Category1/>,<Row1/>,<Row2/>,<Category2/>,<Row3/>,</Row4/>]
+        if (product.category !== lastCategory) {
+            rows.push(
+                <ProductCategoryRow
+                    category={product.category}
+                    key={product.category}/>
+            );
+        }
+        rows.push(
+            <ProductRow
+                product={product}
+                key={product.name}/>
+        );
+        lastCategory = product.category;
+    });
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th>产品</th>
+                <th>价格</th>
+            </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+        </table>
+    );
+  }
+}
+
+class ProductCategoryRow extends Component {
+  render() {
+    var category = this.props.category;
+    return (<tr>
+        <th cols={2}>{category}
+        </th>
+    </tr>)
+  }
+}
+
+class ProductRow extends Component {
+  render() {
+    const product = this.props.product;
+    const name = product.stocked ?
+        product.name :
+        <span style={{color: 'red'}}>
+            {product.name}
+        </span>;
+    // 如果是stocked就直接输出，否则用span包裹并设置style的color为red
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>{product.price}</td>
+        </tr>
+    );
+  }
+}
+
+const PRODUCTS = [
+  {category: '运动商品', price: '$49.99', stocked: true, name: '足球'},
+  {category: '运动商品', price: '$9.99', stocked: true, name: '棒球'},
+  {category: '运动商品', price: '$29.99', stocked: false, name: '篮球'},
+  {category: '电子产品', price: '$99.99', stocked: true, name: 'OPPO R10'},
+  {category: '电子产品', price: '$399.99', stocked: false, name: 'iPhone 8'},
+  {category: '电子产品', price: '$199.99', stocked: true, name: 'MI 8'}
+];
+
+export default Test;
